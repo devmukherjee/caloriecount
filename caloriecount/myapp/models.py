6b2e6361649a45b2c1427f6a0bc8a690020c687c
devmukherjee <devmukherjeeindia@gmail.com>
2023-07-19 16:33:50 +0530

    myapp: Adding Food model

--- caloriecount/myapp/models.py
+++ caloriecount/myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Food(models.Model):
+ 
+    def __str__(self):
+        return self.name
+    
+    name = models.CharField(max_length=100)
+    carbs = models.FloatField()
+    protein = models.FloatField()
+    fats = models.FloatField()
+    calories = models.IntegerField()
